9a73b949fc14264929b3a69d69a71bb344fcbbc0
sc331912 <snehachand1912@gmail.com>
2026-02-05 08:55:32 +0530

    admin page done

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,7 @@ import {
   UserButton,
 } from '@clerk/nextjs'
 import UserSync from "@/components/UserSync";
+import TanStackProvider from "@/components/providers/TanStackProvider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <TanStackProvider>
     <ClerkProvider
     appearance={{
           variables: {
@@ -53,5 +55,6 @@ export default function RootLayout({
       </body>
     </html>
     </ClerkProvider>
+    </TanStackProvider>
   );
 }
